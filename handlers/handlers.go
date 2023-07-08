29eaab1b22e55e4e1d1e2bb68972f28d6cacd4f8
hal <haln_01@proton.me>
2023-07-08 11:31:10 -0700

    feat: parse jwt

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"errors"
+
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/hn275/eng_comp_server/db"
+	"github.com/hn275/eng_comp_server/lib"
 	"gorm.io/gorm"
 )
 
@@ -11,4 +15,20 @@ type Handler struct {
 
 func New() *Handler {
 	return &Handler{db.New()}
+}
+
+func ParseJwt(t string) (*Token, error) {
+	token, err := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+		return []byte(lib.JwtSecret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Token); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
 }
