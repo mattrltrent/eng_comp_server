e4591ab131e559e343cf174f60165ccfb1b61a06
hal <haln_01@proton.me>
2023-07-08 13:13:05 -0700

    log errpr

--- handlers/create_post.go
+++ handlers/create_post.go
@@ -2,11 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/hn275/eng_comp_server/db"
+	"github.com/hn275/eng_comp_server/lib"
 )
 
 type Post struct {
@@ -21,20 +20,18 @@ func (d *Handler) CreatePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var post Post
 	// get body
+	var post Post
 	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
-		fmt.Fprint(os.Stderr, err)
 		w.WriteHeader(http.StatusBadRequest)
+		lib.Err(w, err.Error())
 		return
 	}
 
 	// save to db
-
 	if err := d.Table(db.TablePosts).Create(&post).Error; err != nil {
-		fmt.Fprint(os.Stderr, err)
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(map[string]string{"error": "duplicate email"})
+		lib.Err(w, err.Error())
 		return
 	}
 
